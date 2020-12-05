0bac95e04956b7f3947a466f791e30735a75515c
shie1 <sonkolybence@outlook.hu>
2020-12-05 15:55:42 +0100

    Update

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron');
+const path = require('path');
+const fs = require('fs')
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+    app.quit();
+}
+
+try {
+    fs.readFileSync('./credits.txt')
+    fs.readdirSync('./downloads')
+} catch (error) {
+    try {
+        fs.mkdirSync('./downloads')
+    } catch (error) {}
+    try {
+        fs.writeFileSync('./credits.txt', '| 2020 December 5\n| Design and development: Shie1\n| Electron app template: Fireship (https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA)\n| Audio & Video reencoding source code: TimeForANinja (https://github.com/TimeForANinja)\n| Icons: Smashicons from flaticon.com\n| Made with:\n    nodejs\n    ffmpeg\n    jquery\n    ytdl-core\n    electron\n    electron-forge')
+    } catch (error) {}
+}
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        transparent: true,
+        frame: false,
+        resizable: false,
+        webPreferences: {
+            // devTools: false,
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/render.js
@@ -0,0 +1,183 @@
+const {
+    remote,
+    clipboard
+} = require('electron')
+const path = require('path');
+const fs = require('fs')
+const ytdl = require('ytdl-core')
+const cp = require('child_process');
+const ffmpeg = require('ffmpeg-static');
+
+let tracker
+
+function copy(text) {
+    if (!navigator.clipboard) {
+        fallbackCopyTextToClipboard(text);
+        return;
+    };
+    navigator.clipboard.writeText(text);
+}
+
+function alert(text, ms) {
+    $('#alert').text(text)
+    $('.content').css('height', 'calc(100vh - var(--header-size) * 2')
+    if (!ms) {
+        return
+    }
+    setTimeout(() => {
+        alertS()
+    }, ms)
+}
+
+function alertS() {
+    $('.content').css('height', 'calc(100vh - var(--header-size) * 1')
+    setTimeout(() => {
+        $('#alert').text('')
+    }, 100)
+}
+
+$('#closeApp').click(() => {
+    $('footer').remove();
+    $('.content').css('animation', 'wrap 0.5s infinite')
+    setTimeout(() => {
+        $('.content').remove();
+        $('header').css('animation', 'close 0.5s infinite')
+        setTimeout(() => {
+            $('header').remove()
+            window.close()
+        }, 450)
+    }, 450);
+})
+
+$(".content").on("dragover", function(event) {
+    if (tracker) {
+        return;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    $(this).addClass('dragging');
+});
+
+$(".content").on("dragleave", function(event) {
+    if (tracker) {
+        return;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    $(this).removeClass('dragging');
+});
+
+$(".content").on("drop", async function(event) {
+    if (tracker) {
+        return;
+    }
+    event.preventDefault();
+    event.stopPropagation();
+    $(this).removeClass('dragging')
+    link = event.originalEvent.dataTransfer.getData('url')
+    convert(link)
+
+});
+
+$('#goCreds').click(() => {
+    cp.spawn('C:\\windows\\notepad.exe', ['./credits.txt'])
+})
+
+$('#clip').click(() => {
+    if (tracker) {
+        return;
+    }
+    convert(clipboard.readText())
+})
+
+async function convert(link) {
+    if (!ytdl.validateURL(link)) {
+        alert('This is not a YouTube video!', 1500)
+        return;
+    }
+    title = (await ytdl.getBasicInfo(link)).videoDetails.title.replace(/|/g, '').replace(/"/, '')
+    tracker = {
+        start: Date.now(),
+        audio: {
+            downloaded: 0,
+            total: Infinity
+        },
+        video: {
+            downloaded: 0,
+            total: Infinity
+        },
+        merged: {
+            frame: 0,
+            speed: '0x',
+            fps: 0
+        },
+    };
+    audio = ytdl(link, {
+            filter: 'audioonly',
+            quality: 'highestaudio'
+        })
+        .on('progress', (_, downloaded, total) => {
+            tracker.audio = {
+                downloaded,
+                total
+            };
+        });
+    video = ytdl(link, {
+            filter: 'videoonly',
+            quality: 'highestvideo'
+        })
+        .on('progress', (_, downloaded, total) => {
+            tracker.video = {
+                downloaded,
+                total
+            };
+        });
+    progressbar = setInterval(() => {
+        alert(`"${title}" | Running for: ${Math.floor((Date.now() - tracker.start) / 1000)} seconds`);
+    }, 800);
+    ffmpegProcess = cp.spawn(ffmpeg, [
+        // Remove ffmpeg's console spamming
+        '-loglevel', '0', '-hide_banner',
+        // Redirect/enable progress messages
+        '-progress', 'pipe:3',
+        // 0 second audio offset
+        '-itsoffset', '0', '-i', 'pipe:4',
+        '-i', 'pipe:5',
+        // Choose some fancy codes
+        '-c:v', 'libx265', '-x265-params', 'log-level=0',
+        '-c:a', 'flac',
+        // Define output container
+        '-f', 'matroska', 'pipe:6',
+    ], {
+        windowsHide: true,
+        stdio: [
+            /* Standard: stdin, stdout, stderr */
+            'inherit', 'inherit', 'inherit',
+            /* Custom: pipe:3, pipe:4, pipe:5, pipe:6 */
+            'pipe', 'pipe', 'pipe', 'pipe',
+        ],
+    });
+    ffmpegProcess.on('close', () => {
+        clearInterval(progressbar);
+        tracker = undefined
+        setTimeout(() => {
+            alert(`Video successfully downloaded!`, 2000);
+            setTimeout(() => {
+                cp.exec(`start "" "${path.resolve('./downloads')}"`)
+            }, 2000)
+        }, 1000)
+    });
+    ffmpegProcess.stdio[3].on('data', chunk => {
+        // Parse the param=value list returned by ffmpeg
+        const lines = chunk.toString().trim().split('\n');
+        const args = {};
+        for (const l of lines) {
+            const [key, value] = l.trim().split('=');
+            args[key] = value;
+        }
+        tracker.merged = args;
+    });
+    audio.pipe(ffmpegProcess.stdio[4]);
+    video.pipe(ffmpegProcess.stdio[5]);
+    ffmpegProcess.stdio[6].pipe(fs.createWriteStream(`downloads/${title.replace(/[^a-z0-9]/gi, '_').toLowerCase()}.mp4`));
+}
